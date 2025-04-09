393ccd0b978366d691d30a1f586f71422bf878cb
Prashmin Gurung <prashmingrg@gmail.com>
2025-04-09 20:50:14 +1000

    working in landing page

---
+++ components/Hero.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const HeroSection = () => {
+  return (
+    <section>
+      <div className="mx-auto max-w-5xl  ">
+        <p className="text-center font-bold text-3xl sm:text-5xl">
+          Create Forms in Seconds with AI
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
+
+// <p>Create Forms in Seconds with A</p>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,20 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+// import { Geist, Geist_Mono } from "next/font/google";
+import { Cutive } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
+
+const cutive = Cutive({ subsets: ["latin"], weight: "400" });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -26,9 +29,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
+        <body className={`${cutive.className}  antialiased  bg-[#F8F4E1]`}>
           {children}
         </body>
       </html>
